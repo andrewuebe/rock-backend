0a79f7ea150797036e15c2d6ed286a107a6c3b18
Andrew Uebelein <auebelei@skidmore.edu>
2020-04-09 13:59:47 -0500

    Add production env setting

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const path = require('path');
 
 require('dotenv').config();
 
-const port = process.env.PORT || 5000
-
 app.use(cors()); 
 app.use(express.json({
     limit: '50mb',
@@ -36,6 +34,7 @@ const rocksRouter = require('./routes/rocks');
 
 app.use('/rocks', rocksRouter);
 
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
 app.listen(port, function () {
     console.log(`The app is listening on port: ${port}`);
 }); 
